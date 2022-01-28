3edd68c64924d441ff76509254f610e416759024
shoulr <98544017+shoulr@users.noreply.github.com>
2022-01-28 14:03:57 +0800

    Update config.js

--- config.js
+++ config.js
@@ -33,16 +33,7 @@ var config = {
         "祝福您健康长寿，幸福不老！",./imgs/xiaokeai.jpg"
        "HAPPY~~",./imgs/xiaokeai.jpg"
     },
-    // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
-    desc: {
-        turn_on: "开始",
-        play: "音乐",
-        bannar_coming: "颜色",
-        balloons_flying: "好像少点东西",
-        cake_fadein: "蛋糕？",
-        light_candle: "蜡烛？",
-        wish_message: "生日快乐",
-        story: "A MESSAGE FOR YOU",
+
     }
 };
 
